32fe3f57d2250640f96a0fde999555f0b0654d7b
river2056 <chen0625tung@gmail.com>
2021-05-05 02:28:15 +0800

    added try-catch to avoid Exception crash

--- src/mainApp.js
+++ src/mainApp.js
@@ -5,43 +5,48 @@ const bot = require('./bot');
 
 // app logic section
 const fetchCoinSnapShot = async (market, chatId) => {
-    const baseUrl = `https://www.binance.com/zh-TW/trade/${market}?layout=pro&type=spot`;
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(baseUrl);
+    try {
+        const baseUrl = `https://www.binance.com/zh-TW/trade/${market}?layout=pro&type=spot`;
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(baseUrl);
 
-    // close tutorial dialog
-    // document.querySelector("body > div.css-1u2nk9f > div.css-4nl6qi > div.css-4rbxuz > svg");
-    await clickOnElement(page, 'body > div.css-1u2nk9f > div.css-4nl6qi > div.css-4rbxuz > svg');
+        // close tutorial dialog
+        // document.querySelector("body > div.css-1u2nk9f > div.css-4nl6qi > div.css-4rbxuz > svg");
+        await clickOnElement(page, 'body > div.css-1u2nk9f > div.css-4nl6qi > div.css-4rbxuz > svg');
 
-    // switch to bollinger bands and close MA signs
-    // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div.focus-area.css-haugtg > div.css-s0ysx3 > div.css-16pc8ej > div > svg")
-    await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div.focus-area.css-haugtg > div.css-s0ysx3 > div.css-16pc8ej > div > svg');
-    
-    // close MA
-    // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div.css-17nafj > div")
-    await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div.css-17nafj > div');
-    
-    // open boll
-    // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div:nth-child(4) > div")
-    await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div:nth-child(4) > div');
+        // switch to bollinger bands and close MA signs
+        // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div.focus-area.css-haugtg > div.css-s0ysx3 > div.css-16pc8ej > div > svg")
+        await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div.focus-area.css-haugtg > div.css-s0ysx3 > div.css-16pc8ej > div > svg');
+        
+        // close MA
+        // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div.css-17nafj > div")
+        await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div.css-17nafj > div');
+        
+        // open boll
+        // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div:nth-child(4) > div")
+        await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > div > div:nth-child(2) > div:nth-child(4) > div');
 
-    // close sign dialog
-    // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > svg")
-    await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > svg');
+        // close sign dialog
+        // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > svg")
+        await clickOnElement(page, '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.css-1xvpxce > svg');
 
-    // snapshot canvas
-    // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.kline-container.css-vurnku > canvas.scene")
-    const canvas = await page.$eval(
-        '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.kline-container.css-vurnku > canvas.scene', 
-        elem => elem.toDataURL("image/png")
-    );
+        // snapshot canvas
+        // document.querySelector("#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.kline-container.css-vurnku > canvas.scene")
+        const canvas = await page.$eval(
+            '#__APP > div > div > div.css-1k8svrs > div.css-o0ej6l > div:nth-child(2) > div > div > div > div.kline-container.css-vurnku > canvas.scene', 
+            elem => elem.toDataURL("image/png")
+        );
 
-    // replace prefix to properly save image to file
-    fs.writeFileSync(`./output/${market}.png`, canvas.replace(/data:([A-Za-z-+\/]+);base64,/, ""), { encoding: 'base64' });
-    console.log(`done saving ${market}.png`);
-    bot.sendMessage(chatId, `done fetching...${market}`);
-    await browser.close();
+        // replace prefix to properly save image to file
+        fs.writeFileSync(`./output/${market}.png`, canvas.replace(/data:([A-Za-z-+\/]+);base64,/, ""), { encoding: 'base64' });
+        console.log(`done saving ${market}.png`);
+        bot.sendMessage(chatId, `done fetching...${market}`);
+        await browser.close();
+    } catch(e) {
+        console.log('something wrong happened: ', e);
+        bot.sendMessage(chatId, `something wrong happened, please try again! ${e}`);
+    }
 }
 
 const clickOnElement = async (page, selector) => {
